bc57765fa2a73a0bb0d8bbb89ed2fbf73becca84
OmarFaruq967 <rone967@gmail.com>
2023-11-16 21:34:23 +0600

    Hero Section added

---
+++ front-end/src/data/mainSlider.js
@@ -0,0 +1,24 @@
+const mainSlider = [
+    {
+      id: 1,
+      bg: "/bannerSliderImages/435.jpg",
+      title: "Welcome to \n Syner Shop World!",
+    },
+    {
+      id: 2,
+      bg: "/bannerSliderImages/components-photo.jpeg",
+      title: "The Best Shopping in \n Syner Shop shopping world",
+    },
+    {
+      id: 3,
+      bg: "/bannerSliderImages/slider-bg.jpg",
+      title: "Welcome to \n Syner Shop World!",
+    },
+    {
+      id: 4,
+      bg: "/bannerSliderImages/components-photo.jpeg",
+      title: "The Best Shopping in \n Syner Shop shopping world",
+    },
+  ];
+  
+  export default mainSlider;
