2cdf9bdfb25e21b26760efd5549d2fa11f68ebce
jahid <jahidmorol2@gmail.com>
2024-03-21 20:02:47 +0600

    update something

---
+++ back-end/src/app/modules/User/user.constant.ts
@@ -0,0 +1,8 @@
+export const USER_ROLE = {
+  superAdmin: 'superAdmin',
+  student: 'student',
+  faculty: 'faculty',
+  admin: 'admin',
+} as const; // user const for read only access
+
+export const UserStatus = ['in-progress', 'blocked'];
